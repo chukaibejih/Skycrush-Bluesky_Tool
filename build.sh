fe5f4c949730ea3278c96736929b3171316a978d
chukaibejih <chukaibejih@gmail.com>
2024-12-28 20:14:17 +0100

    modified build script

--- build.sh
+++ build.sh
@@ -1,16 +1,10 @@
-
 #!/usr/bin/env bash
 
 set -o errexit  # Exit immediately if a command exits with a non-zero status
 
-# Upgrade pip (uncomment if necessary)
-# pip install --upgrade pip
-
 # Install dependencies from requirements.txt
 pip install -r requirements.txt
 
-flask db init
-
-flask db migrate -m "Initial migration"
-
+# Run database migrations
+flask db migrate -m "Initial migration" || echo "Migration already exists."
 flask db upgrade
